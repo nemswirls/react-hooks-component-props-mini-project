ec4f590b5c3acb3ed9459fefc336dae2a7f7a4bb
Nemanja Stankovic <nemanjas0823@hotmail.com>
2024-11-19 13:51:57 -0500

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,29 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "../components/Header";
+import About from "../components/About";
+import ArticleList from "../components/ArticleList";
 
 console.log(blogData);
-
 function App() {
+  const blogName = "Underreacted"
+  const posts = [
+    { id: 1, title: 'First Article', content: 'This is the content of the first article.' },
+    { id: 2, title: 'Second Article', content: 'This is the content of the second article.' },
+    { id: 3, title: 'Third Article', content: 'This is the content of the third article.' }
+  ];
+
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name= {blogName}/>
+      <About image="https://example.com/logo.png" 
+        about="This is a blog about technology and coding."
+       />
+      <ArticleList posts= {posts}/>
     </div>
   );
+
 }
+
 
 export default App;
